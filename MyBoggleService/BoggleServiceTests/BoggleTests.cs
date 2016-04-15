755721ae1ee20b7b6ba4ba30cb8de82d55f6aac1
EricSM <eric_miramontes@outlook.com>
2016-04-14 23:41:40 -0600

    Began decoupling iis from unittests

--- MyBoggleService/BoggleServiceTests/BoggleTests.cs
+++ MyBoggleService/BoggleServiceTests/BoggleTests.cs
@@ -7,70 +7,18 @@ using System.Diagnostics;
 
 namespace Boggle
 {
-    /// <summary>
-    /// Provides a way to start and stop the IIS web server from within the test
-    /// cases.  If something prevents the test cases from stopping the web server,
-    /// subsequent tests may not work properly until the stray process is killed
-    /// manually.
-    /// </summary>
-    public static class IISAgent
-    {
-        // Reference to the running process
-        private static Process process = null;
-
-        /// <summary>
-        /// Starts IIS
-        /// </summary>
-        public static void Start(string arguments)
-        {
-            if (process == null)
-            {
-                ProcessStartInfo info = new ProcessStartInfo(Properties.Resources.IIS_EXECUTABLE, arguments);
-                info.WindowStyle = ProcessWindowStyle.Minimized;
-                info.UseShellExecute = false;
-                process = Process.Start(info);
-            }
-        }
-
-
-        /// <summary>
-        ///  Stops IIS
-        /// </summary>
-        public static void Stop()
-        {
-            if (process != null)
-            {
-                process.Kill();
-            }
-        }
-    }
     [TestClass]
     public class BoggleTests
     {
-        /// <summary>
-        /// This is automatically run prior to all the tests to start the server
-        /// </summary>
-        [ClassInitialize()]
-        public static void StartIIS(TestContext testContext)
-        {
-            IISAgent.Start(@"/site:""BoggleService"" /apppool:""Clr4IntegratedAppPool"" /config:""..\..\..\.vs\config\applicationhost.config""");
-        }
-
-        /// <summary>
-        /// This is automatically run when all tests have completed to stop the server
-        /// </summary>
-        [ClassCleanup()]
-        public static void StopIIS()
-        {
-            IISAgent.Stop();
-        }
-
+        private static System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
         private RestTestClient client = new RestTestClient("http://localhost:60000/");
         //private RestTestClient client = new RestTestClient("http://bogglecs3500s16.azurewebsites.net/");
         
         [TestMethod]
         public void TestCreateUser1()
         {
+            new BoggleServer(60000);
+
             dynamic expando = new ExpandoObject();
             expando.Nickname = "Gabe";
             Response r = client.DoPostAsync("users", expando).Result;
